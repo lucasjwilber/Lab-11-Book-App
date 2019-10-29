37d43093cf9885ed06805cfea0b5a0549f0c9ea5
Lucas Wilber <lucasjwilber@gmail.com>
2019-10-29 10:20:21 -0700

    set up search route and book obj constructor

--- server.js
+++ server.js
@@ -8,16 +8,38 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({extended:true,}));
+app.use(express.urlencoded({ extended: true, }));
 app.use(express.static('public'));
 
 app.get('/', renderHTML);
-// app.get('/hello', renderHTML);
-// app.post('/search')
+app.post('/search', handleSearch)
 app.get('*', handleError);
 
 function renderHTML(request, response) {
   response.render('pages/index');
+}
+
+function handleSearch(request, response) {
+  const searchType = request.body[0];
+  const searchText = request.body[1];
+  const url = `https://www.googleapis.com/books/v1/volumes?q=in${searchType}+${searchText}`;
+  superagent.get(url)
+    .then(results => {
+      // console.log(results.body.items[0].volumeInfo);
+      const arrayOfResults = results.body.items.map(book => {
+        console.log(book);
+        return new Book(book.volumeInfo);
+      });
+      console.log(arrayOfResults);
+      response.status(200).send(arrayOfResults);
+    });
+}
+
+function Book(obj) {
+  this.image = obj.imageLinks.thumbnail || 'Image not found.';
+  this.title = obj.title || 'Title not found.';
+  this.author = obj.authors || obj.author || ['Author not found.'];
+  this.description = obj.description || 'Description not found.';
 }
 
 function handleError(request, response, error) {
@@ -27,4 +49,4 @@ function handleError(request, response, error) {
 
 
 
-app.listen(PORT, () =>  console.log(`App is listening on port ${PORT}`) );
+app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
